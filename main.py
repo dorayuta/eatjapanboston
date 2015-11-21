5df0ec317c3ec57705ef27031dd09679dbc3afc8
Yuta <yatokato@gmail.com>
2015-11-20 19:15:08 -0800

    Played around with more pages and bootstrap.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import webapp2
 import jinja2
 import os
+import logging
 
 J_ENV = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -9,15 +10,49 @@ J_ENV = jinja2.Environment(
 
 PROJECT_NAME = 'eatJapanBoston'
 
-class MainPage(webapp2.RequestHandler):
+class BaseHandler(webapp2.RequestHandler):
+    def handle_exception(self, exception, debug):
+        # Log the error.
+        logging.exception(exception)
+
+        # Set a custom message.
+        self.response.write('An error occurred.')
+
+        # If the exception is a HTTPException, use its error code.
+        # Otherwise use a generic 500 error code.
+        if isinstance(exception, webapp2.HTTPException):
+            self.response.set_status(exception.code)
+        else:
+            self.response.set_status(500)
+
+class MainPage(BaseHandler):
     def get(self):
     	template = J_ENV.get_template('index.html')
     	template_values = {'project_name':PROJECT_NAME}
     	self.response.headers['Content-Type'] = 'text/html'
     	self.response.write(template.render(template_values))
 
+class AboutPage(BaseHandler):
+	def get(self):
+		template = J_ENV.get_template('about.html')
+		template_values = {'project_name':PROJECT_NAME}
+		self.response.headers['Content-Type'] = 'text/html'
+		self.response.write(template.render(template_values))
+
+class PlacesPage(BaseHandler):
+	def get(self):
+		template = J_ENV.get_template('places.html')
+		template_values = {
+			'project_name':PROJECT_NAME,
+			'places':['a','b','c'],
+		}
+		self.response.headers['Content-Type'] = 'text/html'
+		self.response.write(template.render(template_values))
+ 
 app = webapp2.WSGIApplication([
-	('/', MainPage),
+	(r'/about', AboutPage),
+	webapp2.Route(r'/', handler=MainPage, name='home'),
+	(r'/places', PlacesPage),
 	],
 	debug=True)
 
